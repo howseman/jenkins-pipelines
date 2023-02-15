32deb4cf881e1c02fe36d929129ea9f88d92c247
Howseman <howsmayer@hotmail.com>
2023-02-15 09:33:27 +0100

    Add very first approach

---
+++ pipeline/pipeline-job-definition.groovy
@@ -0,0 +1,28 @@
+job('build-nodejs-app') {
+  parameters {
+    stringParam('APP_NAME', '', 'The application name to be built')
+  }
+  scm {
+    git {
+      remote {
+        url('https://github.com/your/repo')
+      }
+      branch('$JOB_BRANCH_NAME')
+    }
+  }
+  steps {
+    script {
+      def projectName = params.APP_NAME ?: ""
+      pipelineJob("nodejs-app-${projectName}") {
+        definition {
+          cps {
+            script(readFileFromWorkspace('./${projectName}/Jenkinsfile'))
+          }
+        }
+        parameters {
+          string(name: 'JOB_BRANCH_NAME', defaultValue: 'main', description: 'The branch to be built')
+        }
+      }
+    }
+  }
+}
